4e0daf5d48c63f6ec7df71aa03f3b2b8b7c1a6d2
João Manoel Neto <joaaomanooel@gmail.com>
2017-08-08 16:06:44 -0300

    aplicando catch

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -14,7 +14,7 @@ export class RestaurantsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.restaurants= this.restaurantService.restaurants();
+    this.restaurantService.restaurants().subscribe(restaurants => this.restaurants = restaurants);
   }
 
 }
