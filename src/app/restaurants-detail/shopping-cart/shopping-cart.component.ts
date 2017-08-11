96249cac0c2524f351fd2a82dc01f474d12b6519
João Manoel Neto <joaaomanooel@gmail.com>
2017-08-11 16:07:27 -0300

    add e removendo item do carrinho

--- src/app/restaurants-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurants-detail/shopping-cart/shopping-cart.component.ts
@@ -22,4 +22,16 @@ export class ShoppingCartComponent implements OnInit {
   total(): number{
     return this.shoppingCartService.total();
   }
+
+  clear(){
+    this.shoppingCartService.clear();
+  }
+
+  removeItem(item:any){
+    this.shoppingCartService.removeItem(item);
+  }
+
+  addItem(item: any){
+    this.shoppingCartService.addItem(item);
+  }
 }
