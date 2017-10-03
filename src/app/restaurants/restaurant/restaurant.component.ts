e0e3526a21615206a0f8c847d5c45040e3e74110
Unknown <unknown@example.com>
2017-10-03 15:39:22 -0300

    Animando Restaurants

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Restaurant } from './restaurant.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations:[
+    trigger('restaurantAppeared',[
+      state('ready', style({opacity: 1})),
+      transition('void => ready',[
+        style({opacity: 0, transform: 'translate(-60px, -20px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurantState = 'ready';
 
   @Input() restaurant : Restaurant[];
 
@@ -13,5 +25,7 @@ export class RestaurantComponent implements OnInit {
 
   ngOnInit() {
   }
+
+
 
 }
