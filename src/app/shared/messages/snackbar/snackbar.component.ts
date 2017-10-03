2918133942a7ef61729145c62d249bd862d1f642
Unknown <unknown@example.com>
2017-10-03 15:19:01 -0300

    finalizando aanimação snackbar com do e switchMap

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -3,6 +3,8 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 import { NotificationService } from 'app/shared/messages/notification.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap'; 
 
 @Component({
   selector: 'mt-snackbar',
@@ -31,10 +33,12 @@ export class SnackbarComponent implements OnInit {
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message=>{
-      this.message = message;
-      this.snackVisibility = 'visible';
-      Observable.timer(2000).subscribe(timer=> this.snackVisibility = 'hidden')
-    })
+    this.notificationService.notifier
+      .do(message=>{
+        this.message = message;
+        this.snackVisibility = 'visible';
+      }
+    ).switchMap(message => Observable.timer(2000))
+     .subscribe(timer=> this.snackVisibility = 'hidden')
   }
 }
