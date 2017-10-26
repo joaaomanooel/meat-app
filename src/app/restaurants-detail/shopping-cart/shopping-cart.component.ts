b5327b141f75da34087f879bb22e4511b63b3d80
João Manoel <joaaomanooel@gmail.com>
2017-10-26 11:38:23 -0200

    Animação no carrinho

--- src/app/restaurants-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurants-detail/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { animate, style, transition, state, trigger, keyframes } from '@angular/animations';
 import { ShoppingCartService } from './shopping-cart.service';
 
 @Component({
   selector: 'mt-shopping-cart',
-  templateUrl: './shopping-cart.component.html'
+  templateUrl: './shopping-cart.component.html',
+  animations: [
+    trigger('row', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', animate('300ms 0s ease-in', keyframes([
+        style({opacity:0, transform: 'translateX(-30px)', offset: 0}),
+        style({opacity:0.7, transform: 'translateX(15px)', offset: 0.5}),
+        style({opacity:1, transform: 'translateX(0px)', offset: 1}),
+      ]))),
+      transition('ready => void', animate('300ms 0s ease-out', keyframes([
+        style({opacity:1, transform: 'translateX(0px)', offset: 0}),
+        style({opacity:0.7, transform: 'translateX(-15px)', offset: 0.2}),
+        style({opacity:0, transform: 'translateX(30px)', offset: 1}),
+      ])))
+    ])
+  ]
 })
 export class ShoppingCartComponent implements OnInit {
+
+  rowState = 'ready';
 
   constructor(private shoppingCartService: ShoppingCartService) {
 
